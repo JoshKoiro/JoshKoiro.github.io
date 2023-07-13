f1c3a0e579592730487d55f3ddf7239889b8b553
Josh Koiro <JKoiro@rvii.com>
2023-07-12 20:32:35 -0400

    small text updates and finished deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,34 @@
 #!/bin/bash
-cp C:/Users/JKoiro/Downloads/josh\ koiro\ -\ resume-html.zip ./
-unzip ./josh\ koiro\ -\ resume-html.zip -o
-rm ./josh\ koiro\ -\ resume-html.zip
-git add .
-git c
+downloadedFile="C:\\Users\\JKoiro\\Downloads\\josh koiro - resume-html.zip"
+destinationDirectory="$PWD"
+echo $downloadedFile
+
+# check if file has been downloaded
+if [[ -s "$downloadedFile" ]]; then
+    echo "File has been downloaded. Copying to destination directory..."
+    cp "$downloadedFile" "$destinationDirectory"
+    echo "File copied successfully."
+else
+    echo "File has not been downloaded. No code to update."
+    exit 1
+fi
+
+unzip -o "josh koiro - resume-html.zip"
+rm "josh koiro - resume-html.zip"
+rm  "$downloadedFile"
+read -p "Would you like to deploy to Github? (y/n) " deploy
+
+
+# Convert the deploy variable to lowercase for case-insensitive comparison
+deploy=${deploy,,}
+
+if [[ "$deploy" == "y" ]]; then
+    echo "committing and pushing to github."
+    git add .
+    git c
+    git push
+elif [[ "$deploy" == "n" ]]; then
+    echo "local repository updated. Code not deployed."
+else
+    echo "Invalid input. Please enter 'y' or 'n'."
+fi
